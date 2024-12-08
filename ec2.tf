0f55e2075c2c8b0c307035297fa462269511af66
Leonard22-del <leonardmolua20@gmail.com>
2024-12-08 11:40:54 -0600

    add ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,42 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 5.0"
+
+    }
+
+  }
+
+}
+
+ 
+
+# Configure the AWS Provider
+
+provider "aws" {
+
+  region = "us-east-1"
+  
+
+  assume_role {
+
+    role_arn = "arn:aws:iam::471112668079:role/jenkinsRoleadmin"
+
+  }
+
+ 
+
+}
+
+
+
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = ami-0166fe664262f664c
   instance_type = "t3.micro"
 
   tags = {
---
+++ s3.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "example-bucket"
+
+
+    key    = "path/to/state"
+    region = "us-east-1"
+  }
+}
